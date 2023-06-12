36ae9daca017d43ea890d1c66664168f77c900dc
elenagro <ele.vasevska@gmail.com>
2023-06-12 12:43:38 +0200

    auth unfinished

--- server/services/auth/handlers/auth.js
+++ server/services/auth/handlers/auth.js
@@ -3,3 +3,19 @@ const user = require("../../../pkg/user");
 const jwt = require("jsonwebtoken");
 
 require("dotenv").config();
+
+const create = (req, res) => {
+  try {
+    // equality of passwords
+    if (
+      req.body.password.length === 0 ||
+      req.body.password !== req.body.password2
+    ) {
+      return res.status(400).send("Bad Request");
+    }
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send("Internal Server Error");
+  }
+  //   existing user
+};
