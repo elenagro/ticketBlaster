39318569406a6cd8761e751dd4f5135be101a339
elenagro <ele.vasevska@gmail.com>
2023-06-13 12:55:19 +0200

    header unfinished

--- web/src/shared/components/Navigation/Navigation.js
+++ web/src/shared/components/Navigation/Navigation.js
@@ -1,9 +1,48 @@
 import React from "react";
+import { Link } from "react-router-dom/cjs/react-router-dom";
 import Header from "./Header";
 import styles from "./Navigation.module.css";
+import logo from "../../../assets/Path 1.svg";
+import cart from "../../../assets/cart-shopping-solid.svg";
 
 const Navigation = (props) => {
-  return <Header></Header>;
+  return (
+    <Header>
+      <ul className={styles["left-side"]}>
+        <li>
+          <Link to="/">
+            <img src={logo} className={styles.logo} />
+          </Link>
+          <li>
+            <Link to="/concerts" className={styles["nav-link"]}>
+              Musical Concerts
+            </Link>
+          </li>
+          <li>
+            <Link to="/comedies" className={styles["nav-link"]}>
+              Stand-up Comedy
+            </Link>
+          </li>
+        </li>
+      </ul>
+
+      <ul className={styles["right-side"]}>
+        <li>
+          <input placeholder="Search" className={styles["search-bar"]} />
+        </li>
+        <li>
+          <Link to="#">
+            <img src={cart} className={styles.cart} />
+          </Link>
+        </li>
+        <li>
+          <Link to="#">
+            <img src="../../../assets/user-solid.png" />
+          </Link>
+        </li>
+      </ul>
+    </Header>
+  );
 };
 
 export default Navigation;
