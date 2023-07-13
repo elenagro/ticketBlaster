937fcba162146b9c1fc2567a4f5399b4a82981f6
elenagro <ele.vasevska@gmail.com>
2023-07-13 13:05:43 +0200

    ticket handler

--- server/services/ecommerce/handlers/ticketHandler.js
+++ server/services/ecommerce/handlers/ticketHandler.js
@@ -2,11 +2,51 @@ const Ticket = require("../../../pkg/models/ticketModel");
 
 const createTicket = async (req, res) => {
   try {
-    const { event_id, quantity, user_id } = req.body;
+    const { eventId, quantity, userId } = req.body;
+
+    const ticket = new Ticket({
+      event_id: eventId,
+      quantity: quantity,
+      user_id: userId,
+    });
+
+    await ticket.save();
+
+    res.status(200).send(ticket);
   } catch (err) {
     console.log(err);
     res.status(500).send("Internal Server Error");
   }
 };
 
-module.exports = { createTicket };
+const updateIsPurchased = async (req, res) => {
+  try {
+    const { ticketId } = req.params;
+
+    const ticket = await Ticket.findByIdAndUpdate(
+      ticketId,
+      { is_purchased: true },
+      { new: true }
+    );
+
+    res.status(200).send(ticket);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Internal Server Error");
+  }
+};
+
+const removeCartTicket = async (req, res) => {
+  try {
+    const { ticketId } = req.params;
+
+    await Ticket.findByIdAndRemove(ticketId);
+
+    res.status(204).send();
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Internal Server Error");
+  }
+};
+
+module.exports = { createTicket, updateIsPurchased, removeCartTicket };
