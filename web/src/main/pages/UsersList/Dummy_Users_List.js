70e1e344259cbcd684bf2ce129e0c4df9dd189a3
elenagro <ele.vasevska@gmail.com>
2023-07-30 17:13:39 +0200

    mama - unfinished - users list

---
+++ web/src/main/pages/UsersList/Dummy_Users_List.js
@@ -0,0 +1,11 @@
+export const USERS = [
+  {
+    id: 1,
+    full_name:
+      "https://upload.wikimedia.org/wikipedia/commons/c/cb/Classical_spectacular10.jpg",
+    title: "WIN WIN",
+    date: "July 9th, 2023",
+    text: "Zombie ipsum reversus ab viral inferno nam rick sunt, zombi brains attonitos canum contagium Expansis Adlard max, contagium Nescio hway Vivens undead zombies portae. Sicut voodoo kill Hi ab an mortuos et, infect iam Panduntur guts violent Forsitan et bello, auras horribilem mortui ambulabunt dead Cum. ",
+    location: "Skopje, North Macedonia",
+  },
+];
---
+++ web/src/main/pages/UsersList/UsersList.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import Avatar from "../../../shared/UI/Avatar";
+import Button from "../../../shared/FormElements/Button";
+
+const UsersList = () => {
+  return;
+};
+
+export default UsersList;
